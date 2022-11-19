18b39261568aceac9fbe3ffb1197b353eb1316cf
codertahsin <ahnaftahsin11@gmail.com>
2022-11-19 23:17:21 +0600

    prtoject file

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.main`
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  background-color: black;
+`
+
+export default Container;
